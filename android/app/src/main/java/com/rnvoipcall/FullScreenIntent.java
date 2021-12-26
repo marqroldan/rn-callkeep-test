f69ae2521d6df3f0bd7bca91e47949457e0ee4f0
Marq Roldan <hcmlroldan@gmail.com>
2021-12-26 16:26:08 +0800

    Created new activity: FullScreenIntent.java

---
+++ android/app/src/main/java/com/rnvoipcall/FullScreenIntent.java
@@ -0,0 +1,14 @@
+package com.rnvoipcall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullScreenIntent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_intent);
+    }
+}
